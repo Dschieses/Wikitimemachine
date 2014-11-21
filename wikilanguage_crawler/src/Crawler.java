44f19b96c2f12e06d171229ee5fc211af70e8787
Dschieses <praederp@outlook.de>
2014-11-21 13:43:54 +0100

    Loops over all pages & categories created

--- wikilanguage_crawler/src/Crawler.java
+++ wikilanguage_crawler/src/Crawler.java
@@ -1,28 +1,13 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
-import util.DbConnector;
+import util.ApiCaller;
 
 public class Crawler {
 
-	public static void main(String[] args) {
-		try {
-			Connection c = DbConnector.getInstance().getDbConnection();
-			Statement st = null;
-			ResultSet rs = null;
-			st = c.createStatement();
-			rs = st.executeQuery("select * from " + "tblnarkosen");
-			while (rs.next()) {
-				System.out.println(rs.getString("Protokollnummer"));
-
-			}
-		} catch (ClassNotFoundException | SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	public static void main(String[] args) throws Exception {
+		// String a =
+		// "http://de.wikipedia.org/w/api.php?format=json&action=query&list=allpages&apfilterredir=nonredirects&aplimit=100&apfrom=%EA%9E%A8";
+		String a = "http://de.wikipedia.org/w/api.php?format=json&action=query&list=allpages&apfilterredir=nonredirects&aplimit=500&apfrom=";
+		ApiCaller api = new ApiCaller();
+		api.start(a);
 
 	}
-
 }
--- wikilanguage_crawler/src/util/DbConnector.java
+++ wikilanguage_crawler/src/util/DbConnector.java
@@ -2,7 +2,9 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbConnector {
 
@@ -28,6 +30,32 @@ public class DbConnector {
 		Class.forName("com.mysql.jdbc.Driver");
 		return DriverManager.getConnection("jdbc:mysql://" + host + ":" + port
 				+ "/" + db, dbUsr, dbPwd);
+	}
+
+	public ResultSet executeQuery(Connection con, String query)
+			throws SQLException {
+
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		return rs;
+	}
+
+	public void close(ResultSet rs) throws SQLException {
+		if (!rs.isClosed()) {
+			rs.close();
+		}
+	}
+
+	public void close(Statement st) throws SQLException {
+		if (!st.isClosed()) {
+			st.close();
+		}
+	}
+
+	public void close(Connection con) throws SQLException {
+		if (!con.isClosed()) {
+			con.close();
+		}
 	}
 
 }
