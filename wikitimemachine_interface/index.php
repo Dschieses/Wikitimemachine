6e52973ed71640cc048f84ea17a17985f3b65f43
Michael Kötting <mi-koetting@t-online.de>
2014-12-15 20:31:47 +0100

    Add support for database structure of wikihistory

--- wikitimemachine_interface/index.php
+++ wikitimemachine_interface/index.php
@@ -1,14 +1,28 @@
 <?php
 
-require_once "include/class.wtmInterfaceV2.php";
+	require_once "include/class.wtmInterfaceV1.php";
+	require_once "include/class.wtmInterfaceV2.php";
 
-$interface = new wtmInterface();
+	if($_GET["v"] == "1") {
+		$interface = new wtmInterfaceV1();
 
-if(isset($_GET["year"])) { $interface->setYear($_GET["year"]); }
-if(isset($_GET["lang"])) { $interface->setLang($_GET["lang"]); }
-if(isset($_GET["cat"])) { $interface->setCat($_GET["cat"]); }
-if(isset($_GET["limit"])) { $interface->setLimit($_GET["limit"]); }
-if(isset($_GET["order"])) { $interface->setOrder($_GET["order"]); }
+		if(isset($_GET["year"])) { $interface->setYear($_GET["year"]); }
+		if(isset($_GET["lang"])) { $interface->setLang($_GET["lang"]); }
+		if(isset($_GET["limit"])) { $interface->setLimit($_GET["limit"]); }
+		if(isset($_GET["order"])) { $interface->setOrder($_GET["order"]); }
 
-$interface->printJson();
+		$interface->printJson();
+	}
+	else {
+		$interface = new wtmInterfaceV2();
+
+		if(isset($_GET["year"])) { $interface->setYear($_GET["year"]); }
+		if(isset($_GET["lang"])) { $interface->setLang($_GET["lang"]); }
+		if(isset($_GET["cat"])) { $interface->setCat($_GET["cat"]); }
+		if(isset($_GET["limit"])) { $interface->setLimit($_GET["limit"]); }
+		if(isset($_GET["order"])) { $interface->setOrder($_GET["order"]); }
+
+		$interface->printJson();
+	}
+
 ?>
