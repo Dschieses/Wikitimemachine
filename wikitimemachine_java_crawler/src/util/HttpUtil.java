d3f5c2e4994915f27744a5a9f7389926a98c220a
Dschieses <praederp@outlook.de>
2014-12-16 12:26:47 +0100

    HTTP client updated
    Redo Get in case of error

--- wikitimemachine_java_crawler/src/util/HttpUtil.java
+++ wikitimemachine_java_crawler/src/util/HttpUtil.java
@@ -1,22 +1,54 @@
 package util;
+
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+
 import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 public class HttpUtil {
 
-	public String sendGet(String url) throws Exception {
-		HttpClient client = HttpClientBuilder.create().build();
-		HttpGet request = new HttpGet(url);
-		HttpResponse response = client.execute(request);
+	public String sendGet(String url) throws IOException {
+		HttpClient client = null;
+		HttpGet request = null;
+		HttpResponse response = null;
 
-		
-		BufferedReader in = new BufferedReader(new InputStreamReader(
-				response.getEntity().getContent()));
-		String inputLine="";
+		while (response == null) {
+			try {
+				client = HttpClientBuilder.create().build();
+				request = new HttpGet(url);
+				response = client.execute(request);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Try again");
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					System.out.println("Thread sleep interrupted");
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				System.out.println("Try again");
+				try {
+					Thread.sleep(50);
+				} catch (InterruptedException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					System.out.println("Thread sleep interrupted");
+				}
+			}
+		}
+
+		BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
+		String inputLine = "";
 		StringBuffer stringBuffer = new StringBuffer();
 
 		while ((inputLine = in.readLine()) != null) {
